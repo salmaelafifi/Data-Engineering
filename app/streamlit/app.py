75ea57f11949dbf785dcb3b12575c3b3685ea07f
AshourHana <ashourhana2003@gmail.com>
2025-12-15 15:10:09 +0200

    streamlit directory moved inside app

---
+++ app/streamlit/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Data Engineering Dashboard")
+st.title("Stock Portfolio Visualization")
+st.write("Streamlit service is running and connected to the Docker network.")
