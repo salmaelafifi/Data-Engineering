2acc6ea89344a405741aff2a31d12590790d127a
AshourHana <ashourhana2003@gmail.com>
2025-12-15 02:00:24 +0200

    combined functions for easier use in airflow

--- app/src/DataIntegration.py
+++ app/src/DataIntegration.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import DataPipline as dp
+from Utils import *
 
 def rename_columns(df, column_name, columns_mapping):
     return df.rename(columns={column_name : columns_mapping}, inplace=True)
@@ -19,5 +20,27 @@ def total_trade_amount(D1):
 def return_wanted_columns(df, wanted_columns):
     return df[wanted_columns]
 
+def integrate_data():
+    stocks_clean = dp.extract_data("/app/clean_data_set/final_daily_stock_prices.csv")
+    dim_customers_clean = dp.extract_data("/app/clean_data_set/final_customer.csv")
+    dim_date = dp.extract_data("/app/data/dim_date.csv")
+    trades_clean = dp.extract_data("/app/clean_data_set/final_trades.csv")
+    dim_stock = dp.extract_data("/app/data/dim_stock.csv")
+
+    D1 = merge_dataframes(trades_clean, dim_stock, on_columns=['stock_ticker'])
+    rename_columns(dim_customers_clean, 'account_type', 'customer_account_type')
+    D1 = merge_dataframes(D1, dim_customers_clean, on_columns='customer_id')
+    rename_columns(dim_date, 'date', 'timestamp')
+    D1 = merge_dataframes(D1, dim_date, on_columns=['timestamp'])
+    rename_columns(stocks_clean, 'date', 'timestamp')
+    stocks_clean_long = melt_dataframe(stocks_clean, id_vars=['timestamp'], value_vars=[col for col in stocks_clean.columns if col != 'timestamp'], var_name='stock_ticker', value_name='stock_price')
+    D1 = merge_dataframes(D1, stocks_clean_long, on_columns=['timestamp', 'stock_ticker'])
+    rename_columns(D1,'liquidity_tier', 'stock_liquidity_tier')
+    rename_columns(D1,'sector', 'stock_sector')
+    rename_columns(D1,'industry', 'stock_industry')
+    D1 = total_trade_amount(D1)
+    final_data_set = return_wanted_columns(D1, ['timestamp', 'customer_id', 'stock_ticker', 'transaction_type' , 'quantity' , 'average_trade_size', 'stock_price', 'total_trade_amount', 'customer_account_type', 'day_name','is_weekend', 'is_holiday', 'stock_liquidity_tier', 'stock_sector', 'stock_industry'])
+
+    return final_data_set
 
 
--- app/src/DataPipline.py
+++ app/src/DataPipline.py
@@ -36,26 +36,24 @@ def impute_missing_data(df: pd.DataFrame) -> pd.DataFrame:
 
     return df_imputed
 
+
 def check_outliers(df, col) -> bool:
-  Q1 = df[col].quantile(0.25)
-  Q3 = df[col].quantile(0.75)
-  IQR = Q3 - Q1
-  outliers = df[(df[col] < (Q1 - 1.5 * IQR)) | (df[col] > (Q3 + 1.5 * IQR))]
-  return (len(outliers)/len(df)) *100 > 10
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+    outliers = df[(df[col] < (Q1 - 1.5 * IQR)) | (df[col] > (Q3 + 1.5 * IQR))]
+    return (len(outliers)/len(df)) *100 > 10
 
 def handle_outliers(df, col, multiplier=1.5):
     df_copy = df.copy()
-
     
     Q1 = df_copy[col].quantile(0.25)
     Q3 = df_copy[col].quantile(0.75)
     IQR = Q3 - Q1
-
     
     lower_cap = Q1 - multiplier * IQR
     upper_cap = Q3 + multiplier * IQR
 
-    
     df_copy[col] = np.where(
         df_copy[col] > upper_cap, upper_cap,
         np.where(df_copy[col] < lower_cap, lower_cap, df_copy[col])
@@ -63,6 +61,17 @@ def handle_outliers(df, col, multiplier=1.5):
 
     return df_copy
 
+def detect_and_handle_outliers(df):
+    numeric_cols = df.select_dtypes(include=['number']).columns
+    df_cleaned_stock_prices = df.copy()
+    for col in numeric_cols:
+        if check_outliers(df_cleaned_stock_prices, col):
+            print(f"Handling outliers for {col}...")
+            df_cleaned_stock_prices = handle_outliers(df_cleaned_stock_prices, col)
+        else:
+            print(f"No significant outliers detected in {col} (<=10%)")
+
+    
 def drop_column(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
     df_copy = df.copy()
     if column_name in df_copy.columns:
